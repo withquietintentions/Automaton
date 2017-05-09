95ea399f7be1ef379072180766abcd1d1697825d
Zoe McCloskey <zmccloskey@Zoe-McCloskeys-iMac.local>
2017-05-09 14:57:29 -0700

    initial commit

---
+++ automaton.py
@@ -0,0 +1,4 @@
+""" Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
+def welcome_statement():
+	# asks user if they want to play the game
+	print "Welcome to Automaton. Would you like to play a game about curiosity and information?"
