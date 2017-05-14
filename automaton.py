3297d1bbfbe5ee7a4d7bee91d0bac4697603b69e
withquietintentions <Zoe@penelope.local>
2017-05-14 14:47:46 -0700

    calling dictionaries succesfully

--- automaton.py
+++ automaton.py
@@ -1,12 +1,23 @@
 """ Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
+import statements_lose_version
+
 def welcome_statement():
 	# asks user if they want to play the game
-	print "Welcome to Automaton. Would you like to play a game about curiosity and information?"
-def leading_statment():
-	#list of first part of statments that act as teasers to learn more
+	raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N")
+	if raw_input.upper() == Y :
+		leading_statment()
+	elif raw_input.upper == N :
+		print "Ok bye"
+	else:
+		print "Please enter Y or N"
+def leading_statement(statements):
+	print statements[0]#list of first part of statments that act as teasers to learn more
 def pair_to_leading_statement():
-	#list of corresponding answer statments
+	pass#list of corresponding answer statments
 def you_lose():
-	#list of versions of saying you lose
+	pass#list of versions of saying you lose
 def you_win():
-	#results of winning
+	pass#results of winning
+
+welcome_statement()
+leading_statement(statements_lose_version)
