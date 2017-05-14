56c42a5cdfda5eb039d7f6ef5a46601e96fd3b0b
withquietintentions <Zoe@penelope.local>
2017-05-14 16:54:27 -0700

    adding raw input, issues

--- automaton.py
+++ automaton.py
@@ -1,17 +1,17 @@
 """ Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
 #import statements_lose_version
 statements_lose_version = [  
-	{"question1" : { 
+	{"question" : { 
 		"synopsis": "Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what atomatons are?",
-		"options" : [ ("Yes".upper, True), ("No".upper, False)],
+		"options" : [ ("Yes", True), ("No".upper, False)],
 		"lose message": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity"
 	}}, 
-	{"question2" : {
+	{"question" : {
 		"synopsis": "Originally pandoras box was not a box. Was it a vase, a jar, or a basket?",
 		"options": [ ("vase".lower, True), ("jar".lower, True), ("basket".lower, True), ('', False)],
 		"lose message": "It was a JAR! You lose and so does humanity"
 	}}, 
-	{"question3" : {
+	{"question" : {
 		"synopsis": "The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)",
 		"options": [ ("Y".lower, True), ("N".lower, False)],
 		"lose message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
@@ -21,7 +21,7 @@ statements_lose_version = [
 
 def welcome_statement():
 	# asks user if they want to play the game
-	welcome_answer= raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N")
+	welcome_answer = raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N")
 	if welcome_answer.upper() == "Y" :
 		leading_statement = True 
 	elif raw_input.upper == "N":
@@ -29,9 +29,11 @@ def welcome_statement():
 	else:
 		print "Please enter Y or N"
 def leading_statement(statements, question_num):
-	print statements[question_num]["question1"]["synopsis"] #prints question 1
-def pair_to_leading_statement():
-	pass#list of corresponding answer statments
+	print statements[question_num]["question"]["synopsis"] 
+	answer = raw_input("")
+	if statements[question_num]["question"]["options"]:
+		print statements[question_num]["question"]["lose message"]
+
 def you_lose():
 	pass#list of versions of saying you lose
 def you_win():
