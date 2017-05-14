6dc10915b86fae20ed05167dd44d262e5532bf01
withquietintentions <Zoe@penelope.local>
2017-05-14 13:38:27 -0700

    making each question into a dicitonary

--- statements_lose_version.py
+++ statements_lose_version.py
@@ -1,15 +1,28 @@
-""" List of leading statements for the version of the game in whihc one loses inevidably"""
-leading_statements_lose_version [
-"Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what Atomatons are?  If so open Pandoras box. Y/N",
-"Originally pandoras box was not a box. I was a vase, a jar, or a basket. Type your guess OR type Nah to not open Pandoras Box.",
-"The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)"
+"""questions, and outcomes"""
+[  Question1 = { 
+"synopsis": "Pandora was created by Hephaistos, the same creator who built atomatons?",
+"options" : [ (Ans1 = True) ]
+
+}
+
+
+]
+
+
+
+("Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what Atomatons are?": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity",
+"It was a jar.")
+("Originally pandoras box was not a box. I was a vase, a jar, or a basket?": "a jar")
+("The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)"
 
 
 
 ]
 
 related_statements_lose_version [
-"Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity",
-
+,
+"...often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
 
 ]
+
+open_the_box_or_not_statements [ "If so open Pandoras box. Y/N", "Type your guess OR type Nah to not open Pandoras Box."]
