8982442f2372461b9598e8a23b4c84aff46cb149
withquietintentions <Zoe@penelope.local>
2017-05-14 12:24:45 -0700

    initial commit

---
+++ statements_lose_version.py
@@ -0,0 +1,15 @@
+""" List of leading statements for the version of the game in whihc one loses inevidably"""
+leading_statements_lose_version [
+"Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what Atomatons are?  If so open Pandoras box. Y/N",
+"Originally pandoras box was not a box. I was a vase, a jar, or a basket. Type your guess OR type Nah to not open Pandoras Box.",
+"The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)"
+
+
+
+]
+
+related_statements_lose_version [
+"Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity",
+
+
+]
