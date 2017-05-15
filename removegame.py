b8e2337758597c63773e0e78930310097a0ed35d
withquietintentions <Zoe@penelope.attlocal.net>
2017-05-14 23:16:29 -0700

    needs ieration solution

--- removegame.py
+++ removegame.py
@@ -2,18 +2,20 @@
 #data
 sad_history_hidden_list =["atomic bombs", "sadness", 'war', 'destruction', "rape", "trauma", "bombs", "racism", "sexism"]
 what_user_wants_to_find = raw_input("What would you like to find and remove from history?")
-position_to_pop = sad_history_hidden_list.index(what_user_wants_to_find)
+what_user_wants_to_find_list = [what_user_wants_to_find]
+
 pop_result = False
 
 def pop_search(sad_history_hidden_list):
+	position_to_pop = sad_history_hidden_list.index(what_user_wants_to_find)
 	sad_history_hidden_list.pop(position_to_pop)
 	print what_user_wants_to_find +" has been removed from history"
 
 for item in sad_history_hidden_list:
-	if item == what_user_wants_to_find:
-		pop_search(sad_history_hidden_list)
-	else:
-		print "That is not in the sad hidden history."
+	if item in what_user_wants_to_find_list:
+ 		pop_search(sad_history_hidden_list)
+		break
+	
 
 
 
