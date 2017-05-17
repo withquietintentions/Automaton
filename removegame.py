97ecf53bf073f8e491ff1be5b91c72ef4af5de9d
withquietintentions <Zoe@penelope.attlocal.net>
2017-05-16 23:45:21 -0700

    removes from list succesfully, needs only point score

--- removegame.py
+++ removegame.py
@@ -1,20 +1,38 @@
-"""In this game there is a hidden history of destruction. The goal is to find all hidden items and remove them"""
+"""In this game there is a hidden history of destruction. The goal is to find all hidden items and remove them. O
+Eventually you can also add to the list nice things. On the other side of the cube the game exists with the lists switchedso you add bad things and remove good ones"""
 #data
-sad_history_hidden_list =["atomic bombs", "sadness", 'war', 'destruction', "rape", "trauma", "bombs", "racism", "sexism"]
-what_user_wants_to_find = raw_input("What would you like to find and remove from history?")
-what_user_wants_to_find_list = [what_user_wants_to_find]
+sad_history_hidden_list =["bombs", "sadness", 'war', "cello"]
+game_in_play = True
 
-pop_result = False
-
-def pop_search(sad_history_hidden_list):
-	position_to_pop = sad_history_hidden_list.index(what_user_wants_to_find)
-	sad_history_hidden_list.pop(position_to_pop)
+def pop_search(sad_list, index):
+	sad_list.pop(index)
 	print what_user_wants_to_find +" has been removed from history"
 
-for item in sad_history_hidden_list:
-	if item in what_user_wants_to_find_list:
- 		pop_search(sad_history_hidden_list)
-		break
+def find_item_in_list(what_user_wants_to_find,sad_history_hidden_list):
+	if what_user_wants_to_find in sad_history_hidden_list:
+		index_what_user_wants_to_find = sad_history_hidden_list.index(what_user_wants_to_find)
+		pop_search(sad_history_hidden_list, index_what_user_wants_to_find)
+	else:
+		print what_user_wants_to_find + " could not be removed from history"
+
+
+while game_in_play:
+	if len(sad_history_hidden_list) > 0:
+		what_user_wants_to_find = raw_input("What would you like to find and remove from history?")
+		find_item_in_list(what_user_wants_to_find,sad_history_hidden_list)
+	else:
+		print "You win."
+		exit()
+
+
+
+
+
+
+	
+	
+
+
 	
 
 
