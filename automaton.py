4d2f1768638fff641068093d8dba2b524d9fc053
withquietintentions <Zoe@penelope.attlocal.net>
2017-05-18 08:47:16 -0700

    resolves import

--- automaton.py
+++ automaton.py
@@ -1,64 +1,56 @@
 """ Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
-#import statements_lose_version
-statements_lose_version = [  
-	{"question" : { 
-		"synopsis": "Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what atomatons are?",
-		"options" : [ ("Yes", True), ("No".upper, False)],
-		"lose message": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity"
-	}}, 
-	{"question" : {
-		"synopsis": "Originally pandoras box was not a box. Was it a vase, a jar, or a basket?",
-		"options": [ ("vase".lower, True), ("jar".lower, True), ("basket".lower, True), ('', False)],
-		"lose message": "It was a JAR! You lose and so does humanity"
-	}}, 
-	{"question" : {
-		"synopsis": "The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)",
-		"options": [ ("Y".lower, True), ("N".lower, False)],
-		"lose message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
-	}} 
-]
+from statements_lose_version import statements_lose_version
+
 total_questions = len(statements_lose_version)
 score = 0 #initial score
 question_number = 0 #question started on
-lets_play = True
+
+
 def welcome_statement():
 	"""Asks user if they want to play the game"""
+	
+	lets_play = True
 	welcome_answer = raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N")
-	global lets_play 
 	if welcome_answer.upper() == "Y" :
-		print "lets play"
+		print "OK let's play!"
 	elif welcome_answer.upper() == "N":
 		print "Ok bye"
-		lets_play= False
 		exit()
-	else:
-		lets_play= False
+	'''else:
 		print "Please enter Y or N"
-def leave_game_or_replay():
-	print "Try Again Soon"
-	exit()
-
-
-
-def leading_statement(statements, question_num):
-	"""Asks question and fields response"""
-	answer = raw_input (statements[question_num]["question"]["synopsis"])
-	global score
-	global lets_play
-	if statements[question_num]["question"]["options"]:
-		print statements[question_num]["question"]["lose message"]
-		print "Your final score was %d" %score
 		lets_play = False
-	else:
-		score += 1 #adds score when not losing
-		question_num += 1 # goes to next question
+		return lets_play'''
+	
+	
+
+def leave_game_or_replay(lets_play):
+	if welcome_statement(lets_play) == False:
+		print "Try Again Soon"
+		exit()
+
+
+
+def leading_statement(statements, question_num,lets_play):
+	"""Asks question and fields response"""
+	if welcome_statement(lets_play) == True:
+		answer = raw_input (statements[question_num]["question"]["synopsis"])
+		global score
+
+		if statements[question_num]["question"]["options"]:
+			print statements[question_num]["question"]["lose message"]
+			print "Your final score was %d" %score
+			welcome_statement(lets_play) == False
+			return lets_play
+		else:
+			score += 1 #adds score when not losing
+			question_num += 1 # goes to next question
 		
 
 
 welcome_statement()
-while lets_play: #and question_num <= total_questions:
-	leading_statement(statements_lose_version,question_number)
-leave_game_or_replay()
+ #and question_num <= total_questions:
+leading_statement(statements_lose_version,question_number,lets_play)
+leave_game_or_replay(lets_play)
 
 
 	
--- statements_lose_version.py
+++ statements_lose_version.py
@@ -1,16 +1,16 @@
 """questions, and outcomes"""
 statements_lose_version = [  
-	{"question1" : { 
+	{"question" : { 
 		"synopsis": "Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what atomatons are?",
-		"options" : [ ("Yes".upper, True), ("No".upper, False)],
+		"options" : [ ("Yes".lower, True), ("No".lower, False)],
 		"lose message": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity"
 	}}, 
-	{"question2" : {
+	{"question" : {
 		"synopsis": "Originally pandoras box was not a box. Was it a vase, a jar, or a basket?",
 		"options": [ ("vase".lower, True), ("jar".lower, True), ("basket".lower, True), ('', False)],
 		"lose message": "It was a JAR! You lose and so does humanity"
 	}}, 
-	{"question3" : {
+	{"question" : {
 		"synopsis": "The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)",
 		"options": [ ("Y".lower, True), ("N".lower, False)],
 		"lose message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
