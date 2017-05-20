b4f4b86d07c9f817f1601dcb391415e3829846d2
withquietintentions <Zoe@penelope.attlocal.net>
2017-05-19 21:52:32 -0700

    better logic

--- automaton.py
+++ automaton.py
@@ -2,55 +2,74 @@
 from statements_lose_version import statements_lose_version
 
 total_questions = len(statements_lose_version)
-score = 0 #initial score
 question_number = 0 #question started on
 
 
-def welcome_statement():
+def welcome_statement(lets_play):
 	"""Asks user if they want to play the game"""
 	
-	lets_play = True
-	welcome_answer = raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N")
+	welcome_answer = raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N ")
 	if welcome_answer.upper() == "Y" :
 		print "OK let's play!"
+		lets_play = True
 	elif welcome_answer.upper() == "N":
 		print "Ok bye"
-		exit()
-	'''else:
-		print "Please enter Y or N"
 		lets_play = False
-		return lets_play'''
-	
-	
-
-def leave_game_or_replay(lets_play):
-	if welcome_statement(lets_play) == False:
-		print "Try Again Soon"
-		exit()
-
-
-
-def leading_statement(statements, question_num,lets_play):
-	"""Asks question and fields response"""
-	if welcome_statement(lets_play) == True:
-		answer = raw_input (statements[question_num]["question"]["synopsis"])
-		global score
-
-		if statements[question_num]["question"]["options"]:
-			print statements[question_num]["question"]["lose message"]
-			print "Your final score was %d" %score
-			welcome_statement(lets_play) == False
-			return lets_play
-		else:
-			score += 1 #adds score when not losing
-			question_num += 1 # goes to next question
+		leave_game()
 		
+	else:
+		lets_play = 2
+		# call function that is a loop so if it is 2, it asks a question again
+		repeat_question()
+	return lets_play
+	
+	
+
+def leave_game():
+	exit()
+
+def repeat_question():
+	# repeat question if lets play 2
 
 
-welcome_statement()
- #and question_num <= total_questions:
-leading_statement(statements_lose_version,question_number,lets_play)
-leave_game_or_replay(lets_play)
+
+def leading_statement(statements, question_num, score):
+	"""Asks question and fields response"""
+
+	answer = raw_input (statements[question_num]["question"]["synopsis"])
+	global score
+
+	if statements[question_num]["question"]["options"]:
+		print statements[question_num]["question"]["lose message"]
+		print "Your final score was %d" %score
+		lets_play == False
+		return lets_play
+	else:
+		score += 1 #adds score when not losing
+		question_num += 1 # goes to next question
+
+
+def main():
+	"""
+	this is your driver 
+	"""
+	score= None
+	lets_play = None
+	play = welcome_statement(lets_play)
+	if play == True:
+		leading_statement( ..... score)
+	elif play == 2:
+		# ask question again
+	 else:
+	 	#exit game
+ 
+ 
+## look in lecture notes for this below
+ # if  ... =='__main__':
+ # 	main()
+
+
+
 
 
 	
