a386ac2fe888f673d9f978fb56c192c83f2aa8bc
withquietintentions <Zoe@penelope.local>
2017-05-21 14:16:53 -0700

    imports api

--- automaton.py
+++ automaton.py
@@ -1,10 +1,9 @@
 """ Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
 from statements_lose_version import statements_lose_version
 
-statements = statements_lose_version
-total_questions = len(statements_lose_version)
-question_num = 0 #question started on
-score= 0
+
+ #question started on
+
 
 
 
@@ -39,24 +38,27 @@ def leading_statement(statements,question_num,score):
 	answer = raw_input (statements[question_num]["question"]["synopsis"]) # is a string
 	options = statements[question_num]["question"]["options"] # options is a list with a dictionary in it
 	lose_message = statements[question_num]["question"]["lose_message"] # is a string
-
 	for tupletest in options:
 		if answer in tupletest[1]:
-			#print "its in there" + tupletest[0]
+			return tupletest[0] #debugging
 			#checks to see if in possible answers.
-			return tupletest[0]
-			if tupletest[0] == "YES":
-				print statements[question_num]["question"]["lose message"]
+			
+	
+		
+	
+def checks_move_next_question(statements, question_num, answer1, score):
+	if answer1 == "YES":
+				print statements[question_num]["question"]["lose_message"]
 				print "Your final score was %d" %score
-				lets_play == False
 				leave_game()
-			elif tupletest[0] == "NO":
+	elif answer1 == "NO":
 				score += 1 #adds score when not losing
 				question_num += 1 # goes to next question
-		else:
-			print "please type an understandable response"
-			#this could be expanded to print all the options in the lists in the tuples.
-	return score
+
+	else: 
+		print "try another answer"
+
+	return (score, question_num)
 	
 
 
@@ -64,14 +66,19 @@ def main():
 	"""
 	this the your driver 
 	"""
+	statements = statements_lose_version
+	total_questions = len(statements_lose_version)
+	score= 0
 	lets_play = None
+	question_num = 0
 	play = welcome_statement(lets_play)
-	if play == True:
-		leading_statement(statements,question_num,score)
-	elif play == 2:
-		repeat_question(lets_play)
-	else:
-	 	leave_game()
+	while play == True:
+		
+		answer1 = leading_statement(statements,question_num,score)
+		#print answer1
+		score, question_num = checks_move_next_question(statements, question_num, answer1, score)
+	
+	
  
  #runs the main function
 if __name__=='__main__':
--- removegame.py
+++ removegame.py
@@ -1,7 +1,16 @@
 """In this game there is a hidden history of destruction. The goal is to find all hidden items and remove them. O
 Eventually you can also add to the list nice things. On the other side of the cube the game exists with the lists switchedso you add bad things and remove good ones"""
 #data
-sad_history_hidden_list =["bombs", "sadness", 'war', "trauma", "rape", "fear"]
+from apikeys import KEY
+import requests
+import ast
+from pprint import pprint
+url = "http://words.bighugelabs.com/api/2/9c9544b6b2ed603d85b76727c53008d2/war/json"
+response = requests.get(url)
+dict = ast.literal_eval(response.content)
+pprint(dict)
+
+"""sad_history_hidden_list =["bombs", "sadness", 'war', "trauma", "rape", "fear"]
 game_in_play = True
 
 def pop_search(sad_list, index):
@@ -23,7 +32,7 @@ while game_in_play:
 		find_item_in_list(what_user_wants_to_find,sad_history_hidden_list)
 	else:
 		print "You win."
-		exit()
+		exit()"""
 
 
 
