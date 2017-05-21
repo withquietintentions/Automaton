35c1a06a94be88ff207f38fe8518978759007c9a
withquietintentions <Zoe@penelope.local>
2017-05-21 11:54:55 -0700

    changes structure of options

--- automaton.py
+++ automaton.py
@@ -1,13 +1,14 @@
 """ Automaton is a game with a list of unfinished statements. If you want to want know more you lose. Psuedocode"""
 from statements_lose_version import statements_lose_version
 
+statements = statements_lose_version
 total_questions = len(statements_lose_version)
-question_number = 0 #question started on
+question_num = 0 #question started on
+score= 0
 
 
 def welcome_statement(lets_play):
 	"""Asks user if they want to play the game"""
-	
 	welcome_answer = raw_input ("Welcome to Automaton. Would you like to play a game about curiosity and information? Y/N ")
 	if welcome_answer.upper() == "Y" :
 		print "OK let's play!"
@@ -16,11 +17,10 @@ def welcome_statement(lets_play):
 		print "Ok bye"
 		lets_play = False
 		leave_game()
-		
 	else:
 		lets_play = 2
 		# call function that is a loop so if it is 2, it asks a question again
-		repeat_question()
+		repeat_question(lets_play)
 	return lets_play
 	
 	
@@ -28,48 +28,46 @@ def welcome_statement(lets_play):
 def leave_game():
 	exit()
 
-def repeat_question():
-	# repeat question if lets play 2
+def repeat_question(lets_play):
+	print "Please input Y or N"
+	welcome_statement(lets_play)
 
 
-
-def leading_statement(statements, question_num, score):
+def leading_statement(statements,question_num,score):
 	"""Asks question and fields response"""
+	answer = raw_input (statements[question_num]["question"]["synopsis"]) # is a string
+	options = statements[question_num]["question"]["options"] #options is a list with a dictionary in it
+	lose_message = statements[question_num]["question"]["lose_message"] # is a string
 
-	answer = raw_input (statements[question_num]["question"]["synopsis"])
-	global score
-
-	if statements[question_num]["question"]["options"]:
+	if answer not in options:
+		print "please type in one of the following: "
+		#for options in statements[question_num]["question"]["options"]:
+	if statements[question_num]["question"]["options"] == True:
 		print statements[question_num]["question"]["lose message"]
 		print "Your final score was %d" %score
 		lets_play == False
-		return lets_play
+		leave_game()
 	else:
 		score += 1 #adds score when not losing
 		question_num += 1 # goes to next question
+	return score
+	
 
 
 def main():
 	"""
 	this is your driver 
 	"""
-	score= None
 	lets_play = None
 	play = welcome_statement(lets_play)
 	if play == True:
-		leading_statement( ..... score)
+		leading_statement(statements,question_num,score)
 	elif play == 2:
-		# ask question again
-	 else:
-	 	# exit game
+		repeat_question(lets_play)
+	else:
+	 	leave_game()
  
  
 ## look in lecture notes for this below
- # if  ... =='__main__':
- # 	main()
-
-
-
-
-
-	
+if __name__=='__main__':
+	main()
--- statements_lose_version.py
+++ statements_lose_version.py
@@ -2,18 +2,18 @@
 statements_lose_version = [  
 	{"question" : { 
 		"synopsis": "Pandora was created by Hephaistos, the same creator who built atomatons? Do you want to know what atomatons are?",
-		"options" : [ ("Yes".lower, True), ("No".lower, False)],
-		"lose message": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity"
+		"options" : [ (YES,["yes","YES", "Yes", "ok", "yeah", "Y"])(NO,["no", "NO", "no", "no thanks"]) ],
+		"lose_message": "Atomatons are metal statues of animal, men and monsters, from ancient Greek Myths. The best crafted ones can think and feel like humans. You lose and so does all humanity"
 	}}, 
 	{"question" : {
 		"synopsis": "Originally pandoras box was not a box. Was it a vase, a jar, or a basket?",
-		"options": [ ("vase".lower, True), ("jar".lower, True), ("basket".lower, True), ('', False)],
-		"lose message": "It was a JAR! You lose and so does humanity"
+		"options": [ (YES, ["vase", "jar", "basket"]), (NO,[""])],
+		"lose_message": "It was a JAR! You lose and so does humanity"
 	}}, 
 	{"question" : {
 		"synopsis": "The main trouble with cyborgs, of course , is that they are the illegitimate offspring of militarism and patriarchal capitalism...But illegitimate offspring are... Open Pandoras box to read the rest (open/No)",
-		"options": [ ("Y".lower, True), ("N".lower, False)],
-		"lose message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
+		"options": [(YES, ["Y"]), (NO,["N"]) ],
+		"lose_message": "often exceedingly unfaithful to their origins. Their fathers, after all, are inessential - Donna Haraway A Cyborg Manifesto. All of humanity is doomed and you lose."
 	}} 
 ]
 
