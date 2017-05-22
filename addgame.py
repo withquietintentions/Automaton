03bd0cd4437b3e8076f6302aba5bbf526535fdd0
withquietintentions <Zoe@penelope.local>
2017-05-21 19:46:44 -0700

    made append for addgame'

---
+++ addgame.py
@@ -0,0 +1,17 @@
+
+add_history_list =["bombs", "sadness", 'war', "trauma", "rape", "fear"]
+ask= True
+def ask_user(add_history_list):
+	answer_append = raw_input("What would you like us to remember and remove from history? (Type Q to quit.) ")
+	if answer_append == "Q":
+		return add_history_list
+		exit()
+	else:
+		add_history_list.append(answer_append)
+		print answer_append + " has been added to the list of thingns to remember before deleting from history."
+		return add_history_list
+
+while ask:
+	new_history_list = ask_user(add_history_list)
+	print add_history_list #for debugging
+#make it check if the word is in a dicitonary
--- removegame.py
+++ removegame.py
@@ -2,16 +2,26 @@
 Eventually you can also add to the list nice things. On the other side of the cube the game exists with the lists switchedso you add bad things and remove good ones"""
 #data
 from apikeys import KEY
-import requests
+from addgame import add_history_list
+import json
 import ast
+import requests
 from pprint import pprint
 url = "http://words.bighugelabs.com/api/2/9c9544b6b2ed603d85b76727c53008d2/war/json"
 response = requests.get(url)
 dict = ast.literal_eval(response.content)
-pprint(dict)
+pprint(dict.get("noun"))#gets into nouns
+nouns = (dict.get('noun'))
+pprint(nouns.get("syn"))# gets into synonyms
+synonyms = (nouns.get("syn"))
 
-"""sad_history_hidden_list =["bombs", "sadness", 'war', "trauma", "rape", "fear"]
-game_in_play = True
+for item in synonyms:
+	if item in sad_history_hidden_list
+
+
+# if a synonym is checked in list it tells them what they wanted to find has been removed. 
+
+
 
 def pop_search(sad_list, index):
 	sad_list.pop(index)
@@ -26,13 +36,18 @@ def find_item_in_list(what_user_wants_to_find,sad_history_hidden_list):
 		print what_user_wants_to_find + " could not be removed from history"
 
 
+
+
+main(sad_history_hidden_list):
+"""Driver"""
+game_in_play = True
 while game_in_play:
 	if len(sad_history_hidden_list) > 0:
 		what_user_wants_to_find = raw_input("What would you like to find and remove from history?")
 		find_item_in_list(what_user_wants_to_find,sad_history_hidden_list)
 	else:
 		print "You win."
-		exit()"""
+		exit()
 
 
 
